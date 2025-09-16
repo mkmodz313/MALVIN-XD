db8eb248d1094ca32bd57569bb4ac98e2ad85f76
mkmodz313 <hangustudio93@gmail.com>
2025-09-16 10:24:12 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~Y60VgTBZ#8UeOCbMXklI0NgeB53GUHlyqXkqpZ8Qwq6Ggb0-eR_U",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "9203307499677", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "Pakistan/Harare" //put your country timeZone....leave blank if u don't know.
 };
